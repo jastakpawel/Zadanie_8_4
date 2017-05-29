ecca4ac89bafc8fc8b3c395d590024f0ba76ff5e
jastakpawel <innyjastak@gmail.com>
2017-05-29 11:21:17 +0200

    changed comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // scripts.js
 
 var name = prompt('Enter your name'); //Jeśli zacząłbym tę linię od komentarza to wyłączyłbym polecenie
-alert('Hello, ' + name); //To sprawia że wyskakuje alert na stronie
+//To sprawia że wyskakuje alert na stronie  alert('Hello, ' + name); 
 console.log('Hello, ' + name); /* Te zmiany 
 zaobserwujesz 
 w konsoli 
